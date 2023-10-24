b0aeac0019d9bf2328df67892a63e511718df0d1
Юрий Агеев <ageev.y@yandex.ru>
2023-10-24 22:33:19 +0300

    Add url for category

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (
+    CategoryViewSet
+)
+
+router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet, basename='categories')
+
+urlpatterns = [
+    path('', include('router.urls'))
+]
