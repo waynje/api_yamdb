8a130381983ab98a4043d6b937d49acdbb40db28
Юрий Агеев <ageev.y@yandex.ru>
2023-10-25 22:12:05 +0300

    Add settings, genres, validation

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_slug
 
 
 class Category(models.Model):
@@ -10,6 +11,7 @@ class Category(models.Model):
         max_length=50,
         verbose_name='Слаг',
         unique=True,
+        validators=[validate_slug]
     )
 
     class Meta:
@@ -17,5 +19,26 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
         ordering = ('name',)
 
+    def __str__(self):
+        return self.name
+    
+
+class Genre (models.Model):
+    name = models.CharField(
+        max_length=75,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        verbose_name='Слаг',
+        unique=True,
+        validators=[validate_slug]
+    )
+    
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ('name',)
+    
     def __str__(self):
         return self.name
